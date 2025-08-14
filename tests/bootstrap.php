c9db1d0a17bc8e9b71ebd94136cc799b87e3894c
leonar885 <38926668+leonar885@users.noreply.github.com>
2025-08-14 13:34:24 -0500

    Add tests bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+// Minimal bootstrap for PHPUnit tests
+if ( ! defined( 'ABSPATH' ) ) {
+    define( 'ABSPATH', __DIR__ . '/../' );
+}
+
+require_once __DIR__ . '/../includes/class-pricing.php';
